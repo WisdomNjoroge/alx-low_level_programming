0926c00d170b2075d063b5afa5d8a032075cffdd
WisdomNjoroge <mercyn936@gmail.com>
2023-10-10 12:20:41 +0300

    my_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,6 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 
@@ -83,7 +84,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-
 	dog->age = age;
+
 	return (dog);
 }
